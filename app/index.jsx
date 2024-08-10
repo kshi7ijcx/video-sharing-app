5f21f0ba838355dd4d4493987b3704473538c5df
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-08-10 23:47:07 +0530

    routing from first page to sign in page implemented

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,15 @@
 import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
-import CustomButton from '../components/CustomButton'
+import CustomButton from "../components/CustomButton";
 import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
 
 export default function Index() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full h-full px-4 items-center">
+        <View className="w-full min-h-[90vh] justify-center px-4 items-center">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -30,11 +31,15 @@ export default function Index() {
               resizeMode="contain"
             />
           </View>
-          <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
             Where creativity meets innovation: embark on a journey of limitless
             exploration with Aora
           </Text>
-          <CustomButton title="Continue with email" handlePress={()=>{}} containerStyles='w-full mt-7'/>
+          <CustomButton
+            title="Continue with email"
+            handlePress={() => router.push('/sign-in')}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
 
