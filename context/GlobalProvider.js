bb76be78fadf79fd479c0516c9786bb554ae31f2
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-08-21 23:30:05 +0530

    created a global context for user

---
+++ context/GlobalProvider.js
@@ -0,0 +1,12 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const GlobalContext = createContext();
+export const useGlobalContext = () => useContext(GlobalContext);
+
+const GlobalProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  return <GlobalContext.Provider>{children}</GlobalContext.Provider>;
+};
