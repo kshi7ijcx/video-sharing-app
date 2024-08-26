ae21b5d0889a1364a207fceda734c818538eac3e
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-08-26 13:18:09 +0530

    created function to fetch posts

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,25 +4,30 @@ import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getAllPosts } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
 
 const Home = () => {
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+
   const [refreshing, setRefreshing] = useState(false);
+
   const onRefresh = async () => {
     setRefreshing(true);
-    //re call videos
+    await refetch;
     setRefreshing(false);
   };
 
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
-        keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => {
-          <Text className="text-3xl text-white">{item.id}</Text>;
-        }}
-        ListHeaderComponent={() => {
+        data={[{ id: 1 }, { id: 2 }]}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Text className="text-3xl text-white">{item.title}</Text>
+        )}
+        ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
               <View>
@@ -48,8 +53,8 @@ const Home = () => {
               </Text>
               <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
             </View>
-          </View>;
-        }}
+          </View>
+        )}
         ListEmptyComponent={() => (
           <EmptyState
             title="No Videos Found"
--- components/Trending.jsx
+++ components/Trending.jsx
@@ -3,10 +3,10 @@ const Trending = ({ posts }) => {
   return (
     <FlatList
       data={posts}
-      keyExtractor={(item) => item.$id}
-      renderItem={({ item }) => {
-        <Text className="text-3xl text-white">{item.id}</Text>;
-      }}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <Text className="text-3xl text-white">{item.id}</Text>)
+      }
       horizontal
     />
   );
